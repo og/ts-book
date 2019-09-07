af3dae174f1bc5f212f2d9ef4206d4407e9d4972
Nimo <nimo.jser@gmail.com>
2019-09-08 03:46:31 +0800

    :tada: init dir

---
+++ README.md
@@ -0,0 +1,5 @@
+# ts-book
+
+从实战的角度入门 typescript
+
+1. [基础类型](./chapter/1/README.ts)
---
+++ server.js
@@ -0,0 +1,9 @@
+var express  = require('express')
+var open = require('open')
+
+
+app = express()
+app.use(express.static(__dirname + '/output'));
+var port = "9923"
+app.listen(port)
+open('http://127.0.0.1:' + port + '/README.html');
